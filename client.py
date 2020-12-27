9716e335915f0aac1e53eb0645efca0d6d882352
Amitco812 <amitak@post.bgu.ac.il>
2020-12-27 14:26:12 +0200

    added more code

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from socket import *
-# serverName = ‘hostname’
+
+print('Client started, listening for offer requests...')
 server_port = 13117
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.bind(('', server_port))
@@ -7,17 +8,13 @@ clientSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 clientSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
 message, server_address = clientSocket.recvfrom(2048)
-print(message, server_address)
-
-"""
-serverName = 'amitAmir'
-serverPort = 13117
-clientSocket = socket(AF_INET, SOCK_STREAM)
-
-clientSocket.connect((serverName, serverPort))
-sentence = raw_input('Input lowercase sentence: ')
-clientSocket.send(sentence)
-modifiedSentence = clientSocket.recv(1024)
-print('From Server: ', modifiedSentence)
-clientSocket.close()
-"""
+print('Received offer from ,', server_address, 'attempting to connect...')
+length = len(message)
+if length != 7:
+    print('deprecated message!')
+if message[0] != 0xfe or message[1] != 0xed or message[2] != 0xbe or message[3] != 0xef:
+    print('error in message cookie, reject message!')
+if message[4] != 0x2:
+    print('error in message type!')
+suggestedPort = int.from_bytes(message[5:], "big")
+print(suggestedPort)
--- server.py
+++ server.py
@@ -3,10 +3,12 @@ from time import sleep
 
 server_ip = gethostbyname(gethostname())
 
-# send broadcast through udp every one sec
 UDP_DEST_PORT = 13117
 BROADCAST = '255.255.255.255'
-msg_bytes = [0xfe, 0xed, 0xbe, 0xef, 0x2, 50]
+
+# 0xfeedbeef - magic cookie, 0x2 - type, port - 1300 -> 0x0514
+msg_bytes = [0xfe, 0xed, 0xbe, 0xef, 0x02, 0x05, 0x14]
+# convert to byte array
 msg = bytes(msg_bytes)
 print(msg)
 sock_udp = socket(AF_INET, SOCK_DGRAM)  # UDP
@@ -15,19 +17,7 @@ sock_udp.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
 print('Server started, listening on IP address', server_ip)
 
+# send broadcast through udp every one sec
 for x in range(10):
     sleep(1)
     sock_udp.sendto(msg, (BROADCAST, UDP_DEST_PORT))
-
-"""
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
-
-while 1:
-    connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024)
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence)
-    connectionSocket.close()
-"""
